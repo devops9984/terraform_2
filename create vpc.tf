6f553f075e49dfb2c0d934fdb2d8f2dd87b2cad6
devops9984 <amazoncloud0987@gmail.com>
2024-12-05 16:47:08 +0530

    updated

--- create vpc.tf
+++ create vpc.tf
@@ -7,3 +7,33 @@ resource "aws_vpc" "terraform_vpc" {
     Name = "vpc_T"
   }
 }
+
+#subnet 
+resource "aws_subnet" "web_subnet" {
+  vpc_id     = aws_vpc.terraform_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+#api subnet 
+resource "aws_subnet" "api_subnet" {
+  vpc_id     = aws_vpc.terraform_vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+#DB subnet
+resource "aws_subnet" "dbsubnet" {
+  vpc_id     = aws_vpc.terraform_vpc.id
+  cidr_block = "10.0.3.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
