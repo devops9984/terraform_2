90a48fe7abe3e329049b3e51e26c19eab0a21ea6
devops9984 <amazoncloud0987@gmail.com>
2024-12-05 17:11:29 +0530

    updated

--- create vpc.tf
+++ create vpc.tf
@@ -51,3 +51,25 @@ resource "aws_internet_gateway" "terraform_IG" {
     Name = "gateway_T"
   }
 }
+
+# route table 
+resource "aws_route_table" "public_route" {
+  vpc_id = aws_vpc.terraform_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.terraform_IG.id
+  }
+  tags = {
+    Name = "public_route"
+  }
+}
+
+#private routetable 
+resource "aws_route_table" "private_route" {
+  vpc_id = aws_vpc.terraform_vpc.id
+
+  tags = {
+    Name = "private_route"
+  }
+}
